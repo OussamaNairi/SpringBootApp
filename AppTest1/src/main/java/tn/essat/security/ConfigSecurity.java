fca433d1d3813f315f7786d9f91637189be8ab17
Oussama Nairi <nairioussama@live.fr>
2020-11-12 18:18:12 +0100

    update

--- AppTest1/src/main/java/tn/essat/security/ConfigSecurity.java
+++ AppTest1/src/main/java/tn/essat/security/ConfigSecurity.java
@@ -1,5 +1,8 @@
 package tn.essat.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,19 +12,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class ConfigSecurity extends WebSecurityConfigurerAdapter{
-     
+	 @Autowired
+     DataSource dataSource;
+	 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder crypt=cryptMP();
-		auth.inMemoryAuthentication().withUser("salah1").password(crypt.encode("essat")).roles("USER");
-		auth.inMemoryAuthentication().withUser("fatma1").password(crypt.encode("essat")).roles("ADMIN","USER");
+		//System.out.println(crypt.encode("essat"));
+	//	auth.inMemoryAuthentication().withUser("salah1").password(crypt.encode("essat")).roles("USER");
+		//auth.inMemoryAuthentication().withUser("fatma1").password(crypt.encode("essat")).roles("ADMIN","USER");
+	auth.jdbcAuthentication().dataSource(dataSource)
+	.usersByUsernameQuery("select username as principale, password as credentials, active from user where username=?")
+	.authoritiesByUsernameQuery("select user_username as principale,roles_role as role from user_roles where user_username=?")
+	.passwordEncoder(crypt).rolePrefix("ROLE_");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin();
 		http.authorizeRequests().antMatchers("/ajout**/**","/delete**/**").hasRole("ADMIN");
-		
 		http.authorizeRequests().anyRequest().authenticated();
 	}
 	@Bean
--- AppTest1/src/main/webapp/vues/accueil.jsp
+++ AppTest1/src/main/webapp/vues/accueil.jsp
@@ -1,15 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
    <%@ taglib prefix="c"      uri="http://java.sun.com/jsp/jstl/core" %>
+   <%@ taglib prefix="sec"   uri="http://www.springframework.org/security/tags"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
 <c:set var="cxt" value="${pageContext.request.contextPath }" />
- <%@ taglib prefix="sec"   uri="http://www.springframework.org/security/tags"%>
+
 </head>
 <body>
+<sec:authorize access="isAuthenticated()">
+<p>Bienvenue <sec:authentication property="name" /></p>
+( <a href='<c:url value="/logout" />'> se deconnecter</a>)
+</sec:authorize>
 <h2>Liste des Magasins</h2>
 <table border="1">
     <c:forEach items="${liste}" var="m">
